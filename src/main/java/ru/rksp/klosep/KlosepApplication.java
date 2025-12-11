45bbac4fcae7421ae7890715430e9df5b9184dd8
losiik <49565548+losiik@users.noreply.github.com>
2025-12-11 08:04:55 +0300

    init

---
+++ src/main/java/ru/rksp/klosep/KlosepApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.klosep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlosepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlosepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/klosep/KlosepApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.klosep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlosepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
